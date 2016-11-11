9fff6bdabda783ad31a9de14389e3dcf1f861cce
Maxim O. Gusev <maxgusev@gmail.com>
2016-11-11 12:03:25 +0500

    .

--- libs/routes/hash.js
+++ libs/routes/hash.js
@@ -1,24 +1,56 @@
 /**
  * Filename: hash.js
  */
-import redis_client from '../redis_client';
+import redis from 'redis';
+
+import redis_config from '../../config/redis_config.json';
 
 export default class hash {
     constructor() {
+    	this.key_prefix = "hash";
+    	
+    	this.redis_client = new redis.createClient(
+    		{
+    			host: redis_config.host,
+    			port: redis_config.port,
+    			password: redis_config.password
+    		}
+    	);
+
+    	// Configure redis_client actions
+    	this.redis_client.on('error', function(err) {
+    		console.log("Error " + err);
+    	}); 
     }
 
+    /**
+     * function create()
+     */
 	create(req, res) {
-		res.json({ action: "create" });
+		
 	}
 
+	/**
+	 * function get()
+	 */
 	get(req, res) {
-		const tmp = req.params.hash_value
+		var redis_key = this.key_prefix + ":*:" + req.params.hash_value; 
 		
-		redis_client.set("testa", "testb");
-		
-		res.json({ message: "Hello, World!", hash : tmp });
+		this.redis_client.keys(
+			redis_key,
+			function(err, reply) {
+				if(reply === 1) {
+					res.json({ message: reply });
+				} else {
+					res.json({ message: err });
+				} 
+			}
+		);
 	}
 	
+	/**
+	 * function revoke()
+	 */
 	revoke(req, res) {
 		res.json({ action: "revoke" });
 	}
