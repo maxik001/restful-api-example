052e7671356d1eae41ade8bbb018128f99d10c74
Maxim O. Gusev <maxgusev@gmail.com>
2016-11-11 16:07:24 +0500

    .

--- libs/routes/hash.js
+++ libs/routes/hash.js
@@ -3,33 +3,22 @@
  */
 import redis from 'redis';
 
+import api_response from '../../classes/api_response';
+
 import redis_config from '../../config/redis_config.json';
 
 export default class hash {
+	
     constructor() {
+    	this.api_response = new api_response();
+
     	this.key_prefix = "hash";
     	
     	this.redis_client = new redis.createClient(
     		{
     			host: redis_config.host,
     			port: redis_config.port,
-    			password: redis_config.password,
-    			retry_strategy: function (options) {
-    		        if (options.error.code === 'ECONNREFUSED') {
-    		            // End reconnecting on a specific error and flush all commands with a individual error 
-    		            return new Error('The server refused the connection');
-    		        }
-    		        if (options.total_retry_time > 1000 * 60 * 60) {
-    		            // End reconnecting after a specific timeout and flush all commands with a individual error 
-    		            return new Error('Retry time exhausted');
-    		        }
-    		        if (options.times_connected > 10) {
-    		            // End reconnecting with built in error 
-    		            return undefined;
-    		        }
-    		        // reconnect after 
-    		        return Math.max(options.attempt * 100, 3000);
-    		    }    			
+    			password: redis_config.password		
     		}
     	);
 
@@ -56,29 +45,33 @@ export default class hash {
 			redis_key,
 			function(err, reply) {
 				if (err) { 
-					res.json({ 
-						"error": [{
-							"status": 503,
-							"title": "Cant connect to external server",
-							"detail": "Redis server is not available"
-						}]
-					});
+					this.api_response.add_error(
+						1,
+						"Cant connect to external server",
+						503,
+						"Redis server is not available"
+					);
 				} else {
 					if(reply.length == 1) {
-						var redis_key_parts = reply[0].split(":");					
-						res.json({ 
+						var redis_key_parts = reply[0].split(":");
+						this.api_response.set_data("trust");
+						/*
+						this.api_response.set_data({ 
 							"success": true,
 							"email": redis_key_parts[1],
 							"message": "This is valis hash"
 						});
+						*/
 					} else {
-						res.json({ 
+						/*
+						this.api_response.set_data({ 
 							"success": false,
 							"message": "Cant find hash"
 						});
+						*/
 					}
 				}
-				
+				res.json(this.api_response.get());
 			}
 		);
 	}
