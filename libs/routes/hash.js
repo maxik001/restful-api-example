3614a18b4e190580a0e03d34bfe65bbff86ad3da
Maxim O. Gusev <maxgusev@gmail.com>
2016-11-11 14:53:14 +0500

    .

--- libs/routes/hash.js
+++ libs/routes/hash.js
@@ -13,13 +13,29 @@ export default class hash {
     		{
     			host: redis_config.host,
     			port: redis_config.port,
-    			password: redis_config.password
+    			password: redis_config.password,
+    			retry_strategy: function (options) {
+    		        if (options.error.code === 'ECONNREFUSED') {
+    		            // End reconnecting on a specific error and flush all commands with a individual error 
+    		            return new Error('The server refused the connection');
+    		        }
+    		        if (options.total_retry_time > 1000 * 60 * 60) {
+    		            // End reconnecting after a specific timeout and flush all commands with a individual error 
+    		            return new Error('Retry time exhausted');
+    		        }
+    		        if (options.times_connected > 10) {
+    		            // End reconnecting with built in error 
+    		            return undefined;
+    		        }
+    		        // reconnect after 
+    		        return Math.max(options.attempt * 100, 3000);
+    		    }    			
     		}
     	);
 
     	// Configure redis_client actions
     	this.redis_client.on('error', function(err) {
-    		console.log("Error " + err);
+    		console.log("Redis cleint error. " + err);
     	}); 
     }
 
@@ -39,11 +55,30 @@ export default class hash {
 		this.redis_client.keys(
 			redis_key,
 			function(err, reply) {
-				if(reply === 1) {
-					res.json({ message: reply });
+				if (err) { 
+					res.json({ 
+						"error": [{
+							"status": 503,
+							"title": "Cant connect to external server",
+							"detail": "Redis server is not available"
+						}]
+					});
 				} else {
-					res.json({ message: err });
-				} 
+					if(reply.length == 1) {
+						var redis_key_parts = reply[0].split(":");					
+						res.json({ 
+							"success": true,
+							"email": redis_key_parts[1],
+							"message": "This is valis hash"
+						});
+					} else {
+						res.json({ 
+							"success": false,
+							"message": "Cant find hash"
+						});
+					}
+				}
+				
 			}
 		);
 	}
