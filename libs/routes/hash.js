20cb403ed9f8ef1b658d37f5dabeaae0683db552
Maxim O. Gusev <maxgusev@gmail.com>
2016-11-15 13:09:35 +0500

    .

--- libs/routes/hash.js
+++ libs/routes/hash.js
@@ -7,104 +7,14 @@ import json_validator from 'payload-validator';
 
 import api_response from '../../classes/api_response';
 
-import redis_config from '../../config/redis_config.json';
+import redis_client from '../redis_client';
 
 export default class hash {
 	
     constructor() {
     	this.key_prefix = "hash";
-    	
-    	this.redis_client = new redis.createClient(
-    		{
-    			host: redis_config.host,
-    			port: redis_config.port,
-    			password: redis_config.password		
-    		}
-    	);
-
-    	// Configure redis_client actions
-    	this.redis_client.on('error', function(err) {
-    		console.log("Redis cleint error. " + err);
-    	}); 
     }
 
-    /**
-     * Secondary functions  
-     */
-    
-    /**
-     * function clear_hash_keys()
-     * 
-     * err @object
-     * replies @object
-     */
-    clear_hash_keys(err, replies) {
-    	if(replies.length > 0) {
-    		replies.forEach(
-    			function(key) { this.redis_client.del(key); }
-    		);
-		}
-    }
-        
-    /**
-     * function process_hash_key_set()
-     * 
-     * email @string
-     */
-    process_hash_key_set(email) {
-    	// Clear all keys matched with this email
-    	const key_pattern = this.key_prefix+":"+email+":*";
-    	
-		this.redis_client.keys(key_pattern,	this.clear_hash_keys());
-		
-		//this.redis_client.keys(key_pattern,	function(err, reply) {});
-		
-		// Create new key
-		const hash_value = crypto.randomBytes(64).toString('hex'); 
-		const new_key = this.key_prefix+":"+email+":"+hash_value;
-		
-		this.redis_client.set(new_key, "1", this.response_new_key_created(err, reply));
-    }
-    
-    /**
-     * function response_new_key_created(err, reply)
-     */
-    response_new_key_created(err, reply) {
-		var api_response_obj = new api_response();
-		
-		if (err) { 
-			this.response_redis_is_not_available();
-		} else {
-			api_response_obj.set_data({ 
-				"message": "Use this hash to validate registration",
-				"hash": hash_value
-			});						
-		}
-		res.status(201).json(api_response_obj.get());
-		res.end();    	
-    }
-    
-    /**
-     * function response_redis_is_not_available()
-     */
-    response_redis_is_not_available() {
-    	var api_response_obj = new api_response();
-
-    	api_response_obj.add_error(
-			1,
-			"Cant connect to external server",
-			503,
-			"Redis server is not available"
-		);
-    	res.json(api_response_obj.get());
-    	res.end();
-    }
-    
-    
-    /**
-     * Primary funcions
-     */
-    
     /**
      * function create()
      * 
@@ -129,7 +39,49 @@ export default class hash {
 		);
 		
 		if(validate_result.success) {
-			this.process_hash_key_set(req.body.data['email']);
+	    	// Clear all keys matched with this email
+	    	const key_pattern = this.key_prefix+":"+req.body.data['email']+":*";
+	    	
+			redis_client.keys(
+				key_pattern,
+				function(err, replies) {
+			    	if(replies.length > 0) {
+			    		replies.forEach(
+			    			function(key) { redis_client.del(key); }
+			    		);
+					}
+				}
+			);
+			
+			// Create new key
+			const hash_value = crypto.randomBytes(64).toString('hex'); 
+			const new_key = this.key_prefix+":"+req.body.data['email']+":"+hash_value;
+			
+			redis_client.set(
+				new_key, 
+				"1",
+				function(err, reply) {
+					var api_response_obj = new api_response();
+					
+					if (err) { 
+				    	api_response_obj.add_error(
+							1,
+							"Cant connect to external server",
+							503,
+							"Redis server is not available"
+						);
+				    	res.json(api_response_obj.get());
+				    	res.end();					
+					} else {
+						api_response_obj.set_data({ 
+							"message": "Use this hash to validate registration",
+							"hash": hash_value
+						});						
+					}
+					res.status(201).json(api_response_obj.get());
+					res.end();
+				}
+			);
 		} else {
 			res.status(422);
 			res.end();
@@ -142,20 +94,27 @@ export default class hash {
 	get(req, res) {
 		var redis_key = this.key_prefix + ":*:" + req.params.hash_value; 
 		
-		this.redis_client.keys(
+		redis_client.keys(
 			redis_key,
 			function(err, reply) {
 				var api_response_obj = new api_response();
 
 				if (err) {
-					this.response_redis_is_not_available();
+			    	api_response_obj.add_error(
+						1,
+						"Cant connect to external server",
+						503,
+						"Redis server is not available"
+					);
+			    	res.json(api_response_obj.get());
+			    	res.end();	
 				} else {
 					if(reply.length == 1) {
 						var redis_key_parts = reply[0].split(":");
 						api_response_obj.set_data({ 
 							"success": true,
 							"email": redis_key_parts[1],
-							"message": "This is valis hash"
+							"message": "This is valid hash"
 						});
 					} else {
 						api_response_obj.set_data({ 
@@ -174,20 +133,33 @@ export default class hash {
 	 * function revoke()
 	 */
 	revoke(req, res) {
+		if(!req.body.data) {
+			res.status(422);
+			res.end();
+			return;
+		}
 		
-		var redis_key = this.key_prefix + ":maxgusev@gmail.com:*";
+		var expected_payload = {"email": "", "hash": "" };
+		var mandatory_elements = ["email", "hash"];
 		
-		this.redis_client.keys(
-			redis_key,
-			function(err, replies) {
-				if(replies.length > 0) {
-					
-				}
-				res.json(replies);
-			}
+		var validate_result = json_validator.validator(
+			req.body.data,
+			expected_payload,
+			mandatory_elements,
+			false
 		);
-
 		
+		if(validate_result.success) {
+			const redis_key = this.key_prefix+":"+req.body.data['email']+":"+req.body.data['hash'];
+			
+			redis_client.del(redis_key); 
+			
+			res.status(200);
+			res.end();
+		} else {
+			res.status(422);
+			res.end();
+		}
 	}
 }
 
