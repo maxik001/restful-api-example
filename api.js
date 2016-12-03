cf7a3ccf077cd17194fa26fc072e8298b75d9980
Maxim O. Gusev <maxgusev@gmail.com>
2016-12-03 12:25:40 +0500

    Add random generator

--- api.js
+++ api.js
@@ -7,6 +7,7 @@ import express from 'express';
 import json_validator from 'payload-validator';
 
 import * as hash from './libs/routes/hash';
+import * as random from './libs/routes/random';
 
 // Define routes
 const router = express.Router();
@@ -15,5 +16,9 @@ const router = express.Router();
 router.delete('/hash', hash.revoke );
 router.get('/hash/:hash_value', hash.get );
 router.post('/hash', hash.create );
+
+// Random
+router.get('/random', random.get);
+router.get('/random/:count', random.get);
 
 export default router;
