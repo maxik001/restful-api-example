0cf3bc53a032a50f6fd4da2acb27df344c1a493c
maxik001 <maxgusev@gmail.com>
2016-12-07 00:33:50 +0500

    .

--- libs/routes/accounts.js
+++ libs/routes/accounts.js
@@ -7,32 +7,39 @@ import redis_client from '../redis_client';
 import api_response from '../../classes/api_response';
 
 function create(req, res, next) {
-	// Validate input attributes
-	var bodySchema = joi.object().keys({
-		id: joi.number().positive().required(),
-		login: joi.string().email().required(),
-		password: joi.string().required()
-	}).with('id', 'login', 'password');
-
-	var isBodyValid = new Promise(function(resolve, reject) {
-		joi.validate(req.body, bodySchema, function(err, value) {
-			if(err) { reject(); } else { resolve(); }
-		});
-	});
 	
-	isBodyValid.then(function() {
+	validateBody().then(function() {
 		console.log('valid');
 		
 		var newP = new Promise();
 		
 		res.status(200).end();
-	}).catch(function() {
+	}).catch(function(error) {
 		console.log('invalid');
+		console.log(error);
 		res.status(422).end();
 	});
 	
 	function validateBody() {
-		
+		return new Promise(function(resolve, reject) {
+			console.log("in promise");
+			// Validate input attributes
+			var bodySchema = joi.object().keys({
+				id: joi.number().positive().required(),
+				login: joi.string().email().required(),
+				password: joi.string().required()
+			}).with('id', 'login', 'password');
+
+			joi.validate(req.body, bodySchema, function(err, value) {
+				if(err) {
+					var error = new Error();
+					error.code = 422;
+					reject(error); 
+				} else { 
+					resolve(); 
+				}
+			});
+		});
 	}
 	
 }
