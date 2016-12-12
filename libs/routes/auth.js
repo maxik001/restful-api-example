73ec2324c61a2470106d73e61bc5c41b491f7187
maxik001 <maxgusev@gmail.com>
2016-12-12 22:32:47 +0500

    .

--- libs/routes/auth.js
+++ libs/routes/auth.js
@@ -7,6 +7,8 @@ import api_response from '../../classes/api_response';
 
 import redis_client from '../redis_client';
 
+import jwt_config from '../../config/jwt_config.json';
+
 function auth(req, res) {
 	validateBody().then(function() {
 		return new Promise(function(resolve, reject) {
@@ -22,17 +24,41 @@ function auth(req, res) {
 	}).then(function(account_id) {
 		return new Promise(function(resolve, reject) {
 			redis_client.hgetall('accounts:'+account_id, function(redis_error, redis_reply) {
-				console.log("redis_error", redis_error);
-				console.log("redis_reply", redis_reply);
 				if(redis_error) { reject({status: 503}); }
 				else { 
 					if(redis_reply == null) {
 						reject({status: 200, message: {code: '0', title: 'Nothing to do', detail: ''}});
-					} else { resolve(redis_reply); }
+					} else { redis_reply['id'] = account_id; resolve(redis_reply); }
 				} 
 
 			});
 		});
+	}).then(function(account_data) {
+		
+		console.log(account_data);
+
+		
+		const password_hash = md5(req.body.password);
+		if(account_data.login == req.body.login && account_data.password == password_hash) {
+			// Generate JWT
+			const payload = {
+				id: account_data.id,
+				iat: Math.floor(Date.now() / jwt_config.lifeTime)
+			};
+			var token = jwt.sign(payload, jwt_config.secret);
+			
+			const bodyData = {
+				id: account_data.id,
+				jwt: token
+			};
+			
+			var api_response_obj = new api_response();
+			api_response_obj.set_data(bodyData);
+
+			res.status(200).json(api_response_obj.get()).end();
+		} else {
+			throw {status: 200, message: {code: '0', title: 'Nothing to do', detail: ''}};
+		}
 	}).catch(function(error) {
 		console.log("catch error", error);
 		switch(error.status) {
